4d0ce14f52d2685cd0a63f304454d495f056e8ad
Ilya <maikl.maaikl321@gmail.com>
2025-04-16 22:08:39 +1000

    Решил 10 заданий js

---
+++ hw-1.js
@@ -0,0 +1,75 @@
+////1__задание////
+
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+////2__задание////
+
+let firstIphone = 2007;
+alert(firstIphone);
+
+////3__задание////
+
+let creatorName = ('Брендан Эйх');
+alert(creatorName);
+
+////4__задание////
+
+let a = 10;
+let b = 2;
+
+let sum = a + b;
+alert(sum);
+
+let difference = a - b;
+alert(difference);
+
+let product = a * b;
+alert(product);
+
+let quotient = a / b;
+alert(quotient);
+
+////5__задание////
+
+let a = 2;
+let result = a ** 5;
+alert(result);
+
+////6__задание////
+
+let a = 9;
+let b = 2;
+let remainder = a % b;
+alert(remainder);
+
+////7__задание////
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num ++;
+num --;
+alert(num);
+
+////8__задание////
+
+let age = prompt('Сколько вам лет?');
+alert(age);
+
+////9__задание////
+
+const user = {
+    name: 'Mike',
+    age: 21,
+    isAdmin: true
+}
+
+////10__задание////
+
+let userName = prompt('Напишите ваше имя');
+alert(`Привет, ${userName}!`);
